476ec054960cad4365ea8e5103a0ebc93efeee27
hwan001 <woghks7209@gmail.com>
2026-05-23 17:50:36 +0900

    fix: build error 수정

--- web/src/index.js
+++ web/src/index.js
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { REACT_APP_GOOGLE_CLIENT_ID } from "./secret.js";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
+const googleClientId = process.env.REACT_APP_GOOGLE_CLIENT_ID || "";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<GoogleOAuthProvider clientId={REACT_APP_GOOGLE_CLIENT_ID}>
+	<GoogleOAuthProvider clientId={googleClientId}>
 		<React.StrictMode>
 			<App />
 		</React.StrictMode>
